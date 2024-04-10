2b8e3eb1f7657be48046b9f7edcb0e796610d3e0
Ashton Goh <agkh1995@outlook.com>
2024-04-10 22:58:58 +0800

    Replaced contracts with erc1155 instead

--- smart-contract/contracts/CarbonCreditToken.sol
+++ smart-contract/contracts/CarbonCreditToken.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
-contract CarbonCreditToken is ERC20, ERC20Burnable, Ownable {
+contract CarbonCreditToken is ERC1155, ERC1155Burnable, ERC1155Supply, Ownable {
+    
+    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
 
+    // Create counter for reporting period
+    uint256 public reportingPeriod = 0;
     uint256 public pricePerCredit = 1 gwei;
 
     // Events
@@ -14,47 +19,73 @@ contract CarbonCreditToken is ERC20, ERC20Burnable, Ownable {
     event PricePerCreditSet(uint256 pricePerCredit);
     event CreditsBought(address buyer, uint256 amount);
     event CreditsConsumed(address consumer, uint256 amount);
+    event ReportingPeriodUpdated(uint256 reportingPeriod);
 
-    constructor(
-        address initialOwner,
-        string memory name,
-        string memory symbol
-    ) ERC20(name, symbol) Ownable(initialOwner) {}
+    //////////////////////
+    // Public functions
+    ///////////////////// 
 
-    // Owner functions to mint and set the price
+    function buyCredits(uint256 amount) external payable {
+        require(msg.sender == tx.origin, "Only EOA allowed");
 
-    function listCredits(uint256 amount) public onlyOwner {
-        _mint(owner(), amount);
-        emit CreditsListed(amount);
-    }
-
-    function setPricePerCredit(uint256 _pricePerCredit) public onlyOwner {
-        pricePerCredit = _pricePerCredit;
-        emit PricePerCreditSet(_pricePerCredit);
-    }
-
-    // Public functions to buy and consume credits
-
-    function buyCredits(uint256 amount) public payable {
         uint256 cost = amount * pricePerCredit;
         require(msg.value >= cost, "Incorrect payment");
 
         // Ensure the contract has enough tokens to sell
-        require(balanceOf(owner()) >= amount, "Not enough credits in contract to sell");
+        require(
+            balanceOf(owner(), reportingPeriod) >= amount,
+            "Not enough credits in contract to sell"
+        );
 
         // Transfer tokens from the owner to the buyer
-        _transfer(owner(), msg.sender, amount);
+        _safeTransferFrom(owner(), msg.sender, reportingPeriod, amount, "");
 
         // Handle overpayment
         if (msg.value > cost) {
             payable(msg.sender).transfer(msg.value - cost);
         }
+
         emit CreditsBought(msg.sender, amount);
     }
 
-    function consumeCredits(uint256 amount) public {
-        require(balanceOf(msg.sender) >= amount, "Not enough credits");
-        _burn(msg.sender, amount);
+    function consumeCredits(uint256 amount) external {
+        require(
+            balanceOf(msg.sender, reportingPeriod) >= amount,
+            "Not enough credits"
+        );
+        _burn(msg.sender, reportingPeriod, amount);
+
         emit CreditsConsumed(msg.sender, amount);
+    }
+
+    ////////////////////// 
+    // Owner functions
+    /////////////////////
+
+    function listCredits(uint256 amount) public onlyOwner {
+        _mint(owner(), reportingPeriod, amount, "");
+
+        emit CreditsListed(amount);
+    }
+
+    function setPricePerCredit(uint256 _pricePerCredit) public onlyOwner {
+        pricePerCredit = _pricePerCredit;
+
+        emit PricePerCreditSet(_pricePerCredit);
+    }
+
+    function updateReportingPeriod() public onlyOwner {
+        reportingPeriod++;
+
+        emit ReportingPeriodUpdated(reportingPeriod);
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory values
+    ) internal override(ERC1155, ERC1155Supply) {
+        super._update(from, to, ids, values);
     }
 }
--- smart-contract/contracts/CsvCore.sol
+++ smart-contract/contracts/CsvCore.sol
@@ -1,41 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./CarbonCreditToken.sol";
 
-// This ERC721 contact will be used for owners/organizations to register their carbon projects as NFT
-contract CsvCore is ERC721, Ownable {
+// This core contract acts as factory contract for creating carbon credit projects
+contract CsvCore {
 
     // Mapping of project owners to their token(s)
     mapping(address => address[]) public projectTokensByOwner;
 
-    uint256 private _nextTokenId;
-    uint256 private constant START_TOKEN_ID = 1;
-    CarbonCreditTokenFactory public carbonCreditTokenFactory;
+    // Events
+    event ProjectTokenCreated(address indexed owner, address projectTokenAddress);
 
-    event ProjectTokenCreated(address indexed owner, address projectTokenAddress, string name, string symbol);
+    function createCarbonCreditToken(address owner) external {
+        require(msg.sender == tx.origin, "Only EOA allowed");
 
-    constructor () ERC721("CsvCore", "CSV") {}
-
-    function createProject(address owner, string memory name, string memory symbol) public {
-        require(msg.sender == tx.origin, "Only EOA can call this function");
-
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-
-        // Call the CarbonCreditTokenFactory to create a new token for this project
-        address projectTokenAddress carbonCreditTokenFactory.createCarbonCreditToken(owner, name, symbol);
+        // Create new instance of CarbonCreditToken
+        CarbonCreditToken token = new CarbonCreditToken(owner);
 
         // Store the project token address to the owner's address
-        projectTokensByOwner[owner].push(projectTokenAddress);
+        projectTokensByOwner[owner].push(address(token));
 
         // Emit the project token creation event
-        emit ProjectTokenCreated(owner, projectTokenAddress, name, symbol);
+        emit ProjectTokenCreated(owner, address(token));
     }
-
-    function setCarbonCreditTokenFactory(CarbonCreditTokenFactory _carbonCreditTokenFactory) public onlyOwner {
-        carbonCreditTokenFactory = _carbonCreditTokenFactory;
-    }
-
 }
-
